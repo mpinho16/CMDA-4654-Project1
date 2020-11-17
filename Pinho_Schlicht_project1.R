79da3eaa7913565d341789b73edff39c90917ffa
mpinho16 <mpinho16@vt.edu>
2020-11-16 23:37:47 -0500

    Compiled everything into single files

--- Pinho_Schlicht_project1.R
+++ Pinho_Schlicht_project1.R
@@ -80,3 +80,53 @@ myloess <- function(x, y, span = 0.5, degree = 1, show.plot = TRUE) {
   return(res)
 }
 
+# This function is responsible for finding the k nearest neighbors itself
+# dataframe - dataframe whose points we want to predict categories for
+# train - the training dataframe
+# cl_train - the true classification of the training dataframe
+# k - number of nearest neighbors to look for
+predict_categories <- function(dataframe, train, cl_train, k) {
+  # Predicted categories vector for input dataframe which we will return
+  predicted_categories = integer(0)
+  
+  for (i in 1:nrow(dataframe)) { # Does kNN for each data point in dataframe
+    dataframe_row = dataframe[i,] # current data point
+    
+    # We calculate all the distances from the current data point to every point
+    # in the training dataframe using the apply function
+    distances = cdist(dataframe_row, train, metric="manhattan", p = 1)
+    
+    # Retrieves indices of closest k neighbors
+    k_near_nbs_ix = head(sort(distances, index.return = TRUE)$ix, k)
+    
+    # Determines the label of majority of the closest neighbors
+    predicted_category = names(sort(table(cl_train[k_near_nbs_ix]), decreasing=TRUE)[1])
+    
+    # Adds the predicted category for the current dataframe point to a list that
+    # will include all of them
+    predicted_categories[i] = predicted_category
+  }
+  
+  return(predicted_categories)
+}
+
+# This parent function that we call to return predicted categories for the test dataset
+# and accuracy of the kNN algorithm
+# train - the training dataframe
+# test - the testing dataframe
+# cl_train - the true classification of the training dataframe
+# cl_test - the true classification of the training dataframe
+# k - number of nearest neighbors to look for (by default, 3)
+mykNN <- function(train, test, cl_train, cl_test, k=3) {
+  # Here we compute the confusion matrix for our testing dataframe
+  test_predicted_categories <- predict_categories(test, train, cl_train, k)
+  
+  # Here we compute the confusion matrix for our training dataframe
+  # and the accuracy of the predicted categories for it with the true categories
+  confusion = table(test_predicted_categories, cl_test)
+  accuracy = mean(test_predicted_categories == cl_test)
+  
+  return(list("predicted_categories"=test_predicted_categories,
+              "accuracy"=accuracy,"error"=1-accuracy,
+              "confusion"=confusion,"k"=k))
+}
